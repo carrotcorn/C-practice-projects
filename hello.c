b713c089b8535103b7af79990b71fb660a96c644
Eric Bourne <ericbourne6371@gmail.com>
2021-03-28 14:05:19 -0700

    first commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+   //assigning string
+   string name = get_string("What is your name? \n");
+
+   //printing string to terminal
+   printf("Hello, %s\n", name);
+}
---
+++ population.c
@@ -0,0 +1,42 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+   //Determine how long it takes for a population to reach a particular size
+   int startN;
+   int endN;
+   int years;
+   // Prompt for start size thats larger than 9, if less, re prompts user
+   do
+   {
+      startN = get_int("Start size: ");
+   } while (startN < 9);
+
+   // Prompt for end size
+   do
+   {
+      endN = get_int("End size: ");
+   } while (endN < startN);
+
+   // Calculate number of years passed
+   years = 0;
+   // int population = startN;
+
+   // if (startN != endN)
+   // {
+   //     do
+   //     {
+   //         population = population + population / 3 - population / 4;
+   //         years++;
+   //     }
+   //     while (population < endN);
+   // }
+   while (startN < endN)
+   {
+      startN = startN + (startN / 3) - (startN / 4);
+      years++;
+   }
+
+   printf("Years: %i\n", years);
+}
